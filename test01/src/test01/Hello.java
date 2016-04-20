d1d8ce158b797383b9de4f0f44dc6155535580fa
dh <leedh1910@gmail.com>
2016-04-20 15:42:49 +0900

    d

--- test01/src/test01/Hello.java
+++ test01/src/test01/Hello.java
@@ -3,5 +3,6 @@ package test01;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("hello");
+    System.out.println("asdasdsd");
   }
 }
--- test01/src/test01/Hello2.java
+++ test01/src/test01/Hello2.java
@@ -1,6 +1,6 @@
 package test01;
 
-public class Hello {
+public class Hello2 {
   public static void main(String[] args) {
     System.out.println("hello");
   }
